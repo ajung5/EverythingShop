f31002eef65c08f0c1b7d4208addcd16080e4057
Agung Nawawi <agung@fairtech.com.sg>
2023-11-03 08:49:00 +0700

    Initial Commit

---
+++ EverythingShop/EverythingShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  EverythingShopApp.swift
+//  EverythingShop
+//
+//  Created by Agung Nawawi on 03/11/23.
+//
+
+import SwiftUI
+
+@main
+struct EverythingShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
